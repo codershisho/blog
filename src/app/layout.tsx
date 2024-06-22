32b0842376c9afe86041205a679dd5e9e89f3452
codershisho <codershisho@gmail.com>
2024-06-22 23:27:42 +0900

    header footerの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { adventPro, libreBaskerville, notoJP } from "@/utils/font";
+import BlogHeader from "./components/BlogHeader";
+import BlogFooter from "./components/BlogFooter";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Blog",
@@ -16,9 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body className={`${notoJP.variable} ${adventPro.variable} ${libreBaskerville.variable} ${notoJP.className}`}>
-        <div className="max-w-5xl mx-auto p-8">{children}</div>
+    <html lang="ja" className="h-full"> {/* 親要素を画面全体の高さに拡張 */}
+      <body className={`${notoJP.variable} ${adventPro.variable} ${libreBaskerville.variable} ${notoJP.className} flex flex-col h-full`}> {/* flex flex-colを追加 */}
+        <BlogHeader />
+        <div className="flex-1 max-w-5xl mx-auto pt-24">{children}</div> {/* flex-1を追加 */}
+        <BlogFooter />
       </body>
     </html>
   );
