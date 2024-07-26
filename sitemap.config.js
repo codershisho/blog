d34b49c469a19d7bcac1a1733d0a05ba83e4cf76
codershisho <codershisho@gmail.com>
2024-07-26 22:32:44 +0900

    sitemap追加

---
+++ sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://blog-86p.pages.dev', // カスタムドメイン
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
+  outDir: '.vercel/output/static', // 出力先ディレクトリ
+}
